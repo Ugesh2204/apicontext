c48b9219d4401a124fa5d40190243b0665ab6ae8
ugesh <ugesh.roopchand@hotmail.com>
2022-11-02 17:42:12 +0400

    style added

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Search from './Search'
- import Movie from './Movie'
+ import Movies from './Movies'
 
 const Home = () => {
   return (
   <>
     <Search/>
-    <Movie/>
+    <Movies/>
   </>
   )
 }
