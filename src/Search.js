ccd377534d383ffe1821c2e6434834020b90cc2d
ugesh <ugesh.roopchand@hotmail.com>
2022-11-05 09:52:55 +0400

    Search query

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useGlobalContext } from './context'
 
 const Search = () => {
+  const { query, setQuery, isError} = useGlobalContext();
+ 
   return (
-    <div>Search rrr</div>
+  <>
+   <section className='search-section'>
+    <h2>Search Your Favorite Movie</h2>
+    <form action='#' onSubmit={(e) => e.preventDefault()}>
+      <div>
+        <input type='text' placeholder='search here'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+      <div className='card-error'>
+        <p>{isError.show && isError.msg}</p>
+      </div>
+    </form>
+  </section>
+  </>
   )
 }
 
--- src/context.js
+++ src/context.js
@@ -3,7 +3,10 @@
 //We need a consumer = (useContext()) hooks
 import React, { useContext, useEffect, useState } from 'react';
 
-const API_URL = `http://www.omdbapi.com/?apikey=82dd2684&s=titanic`;
+//const API_URL = `http://www.omdbapi.com/?apikey=82dd2684&s=titanic`;
+const API_URL = `http://www.omdbapi.com/?apikey=82dd2684`;
+//const api = process.env.REACT_APP_API_KEY;
+//console.log(api)
 
 const AppContext = React.createContext();
 
@@ -15,6 +18,7 @@ const AppProvider = ({ children }) => {
     const [isLoading, setIsLoading] = useState(true)
     const [movie, setMovie] = useState([]);
     const [isError, setIsError] = useState({ show: "false", msg: "" });
+    const [query, setQuery] = useState("titanic");
 
     const getMovies =  async(url) => {
         try {
@@ -37,11 +41,13 @@ const AppProvider = ({ children }) => {
     }
     //will run once when page load
     useEffect(() => {
-        getMovies(API_URL);
-    }, []);
+        // getMovies(API_URL);
+        getMovies(`${API_URL}&s=${query}`);
+        //when query change run useeffect  [query]
+    }, [query]);
     
 
-    return <AppContext.Provider value={{ isLoading, movie, isError }}>
+    return <AppContext.Provider value={{ isLoading, movie, isError, query, setQuery }}>
         { children }
     </AppContext.Provider>
 };
